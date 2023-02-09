ea56cc93fb92b886104d1a1a337478e1fda7dba0
Krzysztof Sakwerda <ksakwerda@gmail.com>
2023-02-09 09:25:19 +0100

    chore: revamp rollup config (#92)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,11 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import dts from 'rollup-plugin-dts';
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 
-const dts = require('rollup-plugin-dts').default;
-
-const packageJson = require('./package.json');
+import packageJson from './package.json' assert { type: 'json' };
 
 export default [
   {
@@ -33,8 +32,8 @@ export default [
     external: ['react', 'react-dom'],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'src/index.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
+    plugins: [dts({ tsconfig: './tsconfig.build.json' })],
   },
 ];
